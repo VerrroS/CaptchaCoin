bf856af54b19108d66bb5808b0adb9c7c5ea8993
VerrroS <verrros@gmail.com>
2021-08-02 00:07:17 +0200

    add session functionality

--- app.py
+++ app.py
@@ -32,6 +32,11 @@ class User(db.Model):
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route("/")
 @login_required
 def index():
