70d7135e15df8fbd7bbc3763d9dfc5672d7a185b
VerrroS <verrros@gmail.com>
2021-08-05 13:22:08 +0200

    delete functions file

--- static/functions.js
+++ static/functions.js
@@ -1,40 +1,7 @@
-const stats = document.querySelector("#stats");
-const stats_container = document.querySelector(".stats_container");
-const work_input = document.querySelector("#work_input");
-const work_submit = document.querySelector("#work_submit")
 
 function ripple(){
-  console.log("loades");
   $('#robot_ripples').ripples({resolution: 500, dropRadius: 50, perturbance: 0.2});
 }
 
-function open_stats()
-{
-  if (stats_container.classList.contains("inactive"))
-  {
-      stats_container.classList.remove("inactive");
-  }
-  else {
-    stats_container.classList.add("inactive");
-  }
-}
-
-let time
-function timer(e){
-  now = Date.now()
-  if (e.type == "focus")
-  {
-    start = e.timeStamp;
-  }
-  else
-  {
-    end = e.timeStamp;
-    time = Math.round(end - start)/1000;
-    work_submit.value = time;
-  }
-}
 
 window.addEventListener('DOMContentLoaded', ripple)
-stats.addEventListener('click', open_stats);
-work_input.addEventListener('focus', timer);
-work_submit.addEventListener('click', timer);
