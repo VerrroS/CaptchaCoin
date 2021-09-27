3c36aaa2e8b989ed5a079282b2f26452d223938a
VerrroS <verrros@gmail.com>
2021-09-27 12:38:45 +0200

    change price to 1 $

--- db_create.py
+++ db_create.py
@@ -46,7 +46,7 @@ def populate_shop():
     for i in range(6):
           id = i+ 13
           name = "desktop background"
-          price = 0.3
+          price = 1
           img = "/static/background_q.png"
           type = "png"
           duration = 0
