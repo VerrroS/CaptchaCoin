70f30e731b7a894048021b86f8c6d5b54edbe9e8
VerrroS <verrros@gmail.com>
2021-09-27 12:51:30 +0200

    change int to float

--- app.py
+++ app.py
@@ -153,8 +153,8 @@ def validate():
         key_input = request.form.get('key').upper()
         time = request.form.get('time')
         # check if time is reasonable if not set it to 10 seconds
-        if int(time) > 180 or int(time) < 0:
-            time = 10
+        if float(time) > 180 or float(time) < 0:
+            time = "10"
         point = 1
         if current_key == key_input:
             # stting the current key immediately to x to prevent user from submitting multiple times and getting multiple points
